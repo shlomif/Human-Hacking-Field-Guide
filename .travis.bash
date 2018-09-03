2f6521eaf2598f03fe4b6ecdfb8515425b13a465
Shlomi Fish <shlomif@shlomifish.org>
2018-09-03 21:01:29 +0300

    Add a .travis.yml file for Travis-CI.
    Based on https://github.com/shlomif/perl6-CI-Gen and
    https://github.com/shlomif/ci-gen-framework .
    
    See:
    
    * https://en.wikipedia.org/wiki/Travis_CI
    
    * https://en.wikipedia.org/wiki/Continuous_integration .

--- .travis.bash
+++ .travis.bash
@@ -22,7 +22,7 @@ then
 elif test "$cmd" = "before_install"
 then
     sudo apt-get update -qq
-    sudo apt-get install -y ack-grep cpanminus docbook-defguide docbook-xsl libperl-dev libxml-libxml-perl libxml-libxslt-perl make perl tidy xsltproc
+    sudo apt-get install -y ack-grep cpanminus dbtoepub docbook-defguide docbook-xsl libperl-dev libxml-libxml-perl libxml-libxslt-perl make perl tidy xsltproc
     sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
     cpanm local::lib
 elif test "$cmd" = "install"
