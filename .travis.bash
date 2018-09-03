3aee99046bd611ab0c32001dfd1e83a9ec524407
Shlomi Fish <shlomif@shlomifish.org>
2018-09-03 20:33:14 +0300

    Instruct the CI config file to inst missing deps.
    Install missing dependencies that are required by the build or tests.
    
    See:
    
    * https://en.wikipedia.org/wiki/Travis_CI
    
    * https://en.wikipedia.org/wiki/Continuous_integration .

--- .travis.bash
+++ .travis.bash
@@ -37,6 +37,12 @@ elif test "$cmd" = "build"
 then
     export SCREENPLAY_COMMON_INC_DIR="$PWD/screenplays-common"
     cd HHFG/text/
-    make
-    make test
+    m()
+    \{
+        make DBTOEPUB="/usr/bin/ruby $(which dbtoepub)" \
+            DOCBOOK5_XSL_STYLESHEETS_PATH=/usr/share/xml/docbook/stylesheet/docbook-xsl-ns \
+        "$@"
+    \}
+    m
+    m test
 fi
